f4d30d5f8e9861889c9b53252e5faa5e6b461df4
keevee09 <chrisahanning@gmail.com>
2026-01-30 09:23:28 +0100

    initial commit after fork with Ruff suggested format changes
    There are no functional changes here, just changes as suggested by ruff,
    such as changing to the pathlib library, using if statements instead of
    assert, etc.

--- mbinder.py
+++ mbinder.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 # Modified.
 # Original script source:
@@ -8,7 +7,7 @@
 
 # Usage:
 # Run the script from a folder with file "all.mbox"
-# Attachments will be extracted into subfolder "attachments" 
+# Attachments will be extracted into subfolder "attachments"
 # with prefix "m " where m is a message ID in mbox file.
 
 # Or
@@ -28,32 +27,51 @@
 # absence of latent or other defects, accuracy, or the present or
 # absence of errors, whether or not discoverable, all to the
 # greatest extent permissible under applicable law.
-
+import argparse
 import errno
 import mailbox
 import mimetypes
 import os
-import pathlib  # since Python 3.4
 import re
+import sys
 import traceback
 from email.header import decode_header
-import argparse
-import sys
+from pathlib import Path
 
 
-def parse_options(args=[]):
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input', default='all.mbox', help='Input file')
-    parser.add_argument('-o', '--output', default='attachments/', help='Output folder')
-    parser.add_argument('--no-inline-images', action='store_true')
-    parser.add_argument('--start',
-                        type=message_id_type, default=0,
-                        help='On which message to start')
-    parser.add_argument('--stop',
-                        type=message_id_type, default=100000000000,
-                        help='On which message to stop, not included')
-    parser.add_argument('-s', '--sender', default='', help='Filter messages by sender email domain')
-    parser.add_argument('-r', '--recipient', default='', help='Filter messages by sender email domain')
+def parse_options(args=None):
+    if args is None:
+        args = []
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-i", "--input", default="all.mbox", help="Input file")
+    parser.add_argument("-o", "--output", default="attachments/", help="Output folder")
+    parser.add_argument("--no-inline-images", action="store_true")
+    parser.add_argument(
+        "--start",
+        type=message_id_type,
+        default=0,
+        help="On which message to start",
+    )
+    parser.add_argument(
+        "--stop",
+        type=message_id_type,
+        default=100000000000,
+        help="On which message to stop, not included",
+    )
+    parser.add_argument(
+        "-s",
+        "--sender",
+        default="",
+        help="Filter messages by sender email domain",
+    )
+    parser.add_argument(
+        "-r",
+        "--recipient",
+        default="",
+        help="Filter messages by sender email domain",
+    )
     return parser.parse_args(args)
 
 
@@ -61,7 +79,7 @@ def message_id_type(arg):
     try:
         i = int(arg)
     except ValueError as e:
-        raise argparse.ArgumentTypeError(str(e))
+        raise argparse.ArgumentTypeError(str(e)) from None
     if i < 0:
         raise argparse.ArgumentTypeError("Must be greater than or equal 0.")
     return i
@@ -74,15 +92,30 @@ class Extractor:
 
         self.options = options
 
-        assert os.path.isfile(options.input)
+        input_path = Path(options.input)
+        if not input_path.is_file():
+            raise FileNotFoundError(
+                errno.ENOENT,
+                os.strerror(errno.ENOENT),
+                options.input,
+            )
         self.mbox = mailbox.mbox(options.input)
 
-        if not os.path.exists(options.output):
-            os.makedirs(options.output)
+        output_path = Path(options.output)
+        output_path.mkdir(parents=True, exist_ok=True)
 
-        self.inline_image_folder = os.path.join(options.output, 'inline_images/')
-        if (not options.no_inline_images) and (not os.path.exists(self.inline_image_folder)):
-            os.makedirs(self.inline_image_folder)
+        self.inline_image_folder = Path(options.output) / "inline_images/"
+        self.inline_image_folder = (
+            self.inline_image_folder.resolve()
+        )  # Resolve the absolute path
+        if (not options.no_inline_images) and (not self.inline_image_folder.exists()):
+            self.inline_image_folder.mkdir(parents=True, exist_ok=True)
+
+        # self.inline_image_folder = os.path.join(options.output, "inline_images/")
+        # if (not options.no_inline_images) and (
+        #     not os.path.exists(self.inline_image_folder)
+        # ):
+        #     os.makedirs(self.inline_image_folder)
 
     def increment_total(self):
         self.__total += 1
@@ -98,24 +131,28 @@ class Extractor:
 
 
 def to_file_path(save_to, name):
-    return os.path.join(save_to, name)
+    return Path(save_to) / name
+
+
+MAX_EXTENSION_LENGTH = 20
 
 
 def get_extension(name):
-    extension = pathlib.Path(name).suffix
-    return extension if len(extension) <= 20 else ''
+    path = Path(name)
+    extension = path.suffix
+    return extension if len(extension) <= MAX_EXTENSION_LENGTH else ""
 
 
 def resolve_name_conflicts(save_to, name, file_paths, attachment_number):
     file_path = to_file_path(save_to, name)
 
-    START = 1
-    iteration_number = START
+    start = 1
+    iteration_number = start
 
     while os.path.normcase(file_path) in file_paths:
         extension = get_extension(name)
-        iteration = '' if iteration_number <= START else ' (%s)' % iteration_number
-        new_name = '%s attachment %s%s%s' % (name, attachment_number, iteration, extension)
+        iteration = "" if iteration_number <= start else f" ({iteration_number})"
+        new_name = f"{name} attachment {attachment_number}{iteration}{extension}"
         file_path = to_file_path(save_to, new_name)
         iteration_number += 1
 
@@ -124,74 +161,94 @@ def resolve_name_conflicts(save_to, name, file_paths, attachment_number):
 
 
 # Whitespaces: tab, carriage return, newline, vertical tab, form feed.
-FORBIDDEN_WHITESPACE_IN_FILENAMES = re.compile('[\t\r\n\v\f]+')
+FORBIDDEN_WHITESPACE_IN_FILENAMES = re.compile("[\t\r\n\v\f]+")
 OTHER_FORBIDDEN_FN_CHARACTERS = re.compile('[/\\\\\\?%\\*:\\|"<>\0]')
 
 
 def filter_fn_characters(s):
-    result = re.sub(FORBIDDEN_WHITESPACE_IN_FILENAMES, ' ', s)
-    result = re.sub(OTHER_FORBIDDEN_FN_CHARACTERS, '_', result)
+    result = re.sub(FORBIDDEN_WHITESPACE_IN_FILENAMES, " ", s)
+    result = re.sub(OTHER_FORBIDDEN_FN_CHARACTERS, "_", result)
     return result
 
 
 def decode_filename(part, fallback_filename, mid):
     if part.get_filename() is None:
-        print('Filename is none: %s %s.' % (mid, fallback_filename))
+        print(f"Filename is none: {mid} {fallback_filename}")
         return fallback_filename
-    else:
-        decoded_name = decode_header(part.get_filename())
+    decoded_name = decode_header(part.get_filename())
 
-        if isinstance(decoded_name[0][0], str):
-            return decoded_name[0][0]
-        else:
-            try:
-                name_encoding = decoded_name[0][1]
-                return decoded_name[0][0].decode(name_encoding)
-            except:
-                print('Could not decode %s %s attachment name.' % (mid, fallback_filename))
-                return fallback_filename
+    if isinstance(decoded_name[0][0], str):
+        return decoded_name[0][0]
+    try:
+        name_encoding = decoded_name[0][1]
+        return decoded_name[0][0].decode(name_encoding)
+    except UnicodeDecodeError:
+        print(f"Could not decode {mid} {fallback_filename} attachment name.")
+        return fallback_filename
 
 
 def write_to_disk(part, file_path):
-    with open(file_path, 'wb') as f:
+    file_path = Path(file_path)
+    with file_path.open("wb") as f:
         f.write(part.get_payload(decode=True))
 
 
-def save(extractor, mid, part, attachments_counter, inline_image=False):
+def save(extractor, mid, part, attachments_counter, inline_image=None):
+    if inline_image is None:
+        inline_image = False
+
     extractor.increment_total()
 
     try:
         if inline_image:
-            attachments_counter['inline_image'] += 1
-            attachment_number_string = 'ii' + str(attachments_counter['inline_image'])
+            attachments_counter["inline_image"] += 1
+            attachment_number_string = "ii" + str(attachments_counter["inline_image"])
             destination_folder = extractor.inline_image_folder
         else:
-            attachments_counter['value'] += 1
-            attachment_number_string = str(attachments_counter['value'])
+            attachments_counter["value"] += 1
+            attachment_number_string = str(attachments_counter["value"])
             destination_folder = extractor.options.output
 
+        content_type = part.get_content_type()
+        extension = mimetypes.guess_extension(content_type) or ""
+        if not extension:
+            print(f"Warning: Unrecognized MIME type: {content_type}")
+            extension = ".txt"
         filename = decode_filename(
             part,
-            attachment_number_string + str(mimetypes.guess_extension(part.get_content_type()) or ''),
-            mid)
+            attachment_number_string + str(extension),
+            mid,
+        )
 
         filename = filter_fn_characters(filename)
-        filename = '%s %s' % (mid, filename)
+        filename = f"{mid} {filename}"
 
-        previous_file_paths = attachments_counter['file_paths']
+        previous_file_paths = attachments_counter["file_paths"]
 
         try:
-            write_to_disk(part, resolve_name_conflicts(
-                destination_folder, filename,
-                previous_file_paths,
-                attachment_number_string))
+            write_to_disk(
+                part,
+                resolve_name_conflicts(
+                    destination_folder,
+                    filename,
+                    previous_file_paths,
+                    attachment_number_string,
+                ),
+            )
         except OSError as e:
             if e.errno == errno.ENAMETOOLONG:
-                short_name = '%s %s%s' % (mid, attachment_number_string, get_extension(filename))
-                write_to_disk(part, resolve_name_conflicts(
-                    destination_folder, short_name,
-                    previous_file_paths,
-                    attachment_number_string))
+                short_name = (
+                    f"{mid} {attachment_number_string}{get_extension(filename)}"
+                )
+                write_to_disk(
+                    part,
+                    resolve_name_conflicts(
+                        destination_folder,
+                        short_name,
+                        previous_file_paths,
+                        attachment_number_string,
+                    ),
+                )
             else:
                 raise
     except:
@@ -204,20 +261,24 @@ def check_part(extractor, mid, part, attachments_counter):
     if part.is_multipart():
         for p in part.get_payload():
             check_part(extractor, mid, p, attachments_counter)
-    elif (part.get_content_disposition() == 'attachment') \
-            or ((part.get_content_disposition() != 'inline') and (part.get_filename() is not None)):
+    elif (part.get_content_disposition() == "attachment") or (
+        (part.get_content_disposition() != "inline")
+        and (part.get_filename() is not None)
+    ):
         save(extractor, mid, part, attachments_counter)
-    elif (mime_type.startswith('application/') and not mime_type == 'application/javascript') \
-            or mime_type.startswith('model/') \
-            or mime_type.startswith('audio/') \
-            or mime_type.startswith('video/'):
-        message_id_content_type = 'Message id = %s, Content-type = %s.' % (mid, mime_type)
-        if part.get_content_disposition() == 'inline':
-            print('Extracting inline part... ' + message_id_content_type)
+    elif (
+        (mime_type.startswith("application/") and mime_type != "application/javascript")
+        or mime_type.startswith("model/")
+        or mime_type.startswith("audio/")
+        or mime_type.startswith("video/")
+    ):
+        message_id_content_type = f"Message id = {mid}, Content-type = {mime_type}."
+        if part.get_content_disposition() == "inline":
+            print("Extracting inline part... " + message_id_content_type)
         else:
-            print('Other Content-disposition... ' + message_id_content_type)
+            print("Other Content-disposition... " + message_id_content_type)
         save(extractor, mid, part, attachments_counter)
-    elif (not extractor.options.no_inline_images) and mime_type.startswith('image/'):
+    elif (not extractor.options.no_inline_images) and mime_type.startswith("image/"):
         save(extractor, mid, part, attachments_counter, True)
 
 
@@ -225,21 +286,17 @@ def process_message(extractor, mid):
     msg = extractor.mbox.get_message(mid)
 
     if extractor.options.sender:
-        sender = str(msg.get('From', '')).lower()
+        sender = str(msg.get("From", "")).lower()
         if extractor.options.sender.lower() not in sender:
             return
-    
+
     if extractor.options.recipient:
-        recipient = str(msg.get('To', '')).lower()
+        recipient = str(msg.get("To", "")).lower()
         if extractor.options.recipient.lower() not in recipient:
             return
 
     if msg.is_multipart():
-        attachments_counter = {
-            'value': 0,
-            'inline_image': 0,
-            'file_paths': []
-        }
+        attachments_counter = {"value": 0, "inline_image": 0, "file_paths": []}
         for part in msg.get_payload():
             check_part(extractor, mid, part, attachments_counter)
 
@@ -252,14 +309,14 @@ def extract_mbox_file(options):
         try:
             process_message(extractor, i)
         except KeyError:
-            print('The whole mbox file was processed.')
+            print("The whole mbox file was processed.")
             break
         if i % 1000 == 0:
-            print('Messages processed: {}'.format(i))
+            print(f"Messages processed: {i}")
 
     print()
-    print('Total files:  %s' % extractor.get_total())
-    print('Failed:       %s' % extractor.get_failed())
+    print(f"Total files:  {extractor.get_total()}")
+    print(f"Failed:       {extractor.get_failed()}")
 
 
 if __name__ == "__main__":
